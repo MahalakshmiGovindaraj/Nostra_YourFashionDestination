bf8c613862a8126707bec2b75179649d40de54db
MahalakshmiGovindaraj <g.mahalaxmi95@gmail.com>
2025-06-06 02:04:50 +0530

    Added new functionality

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function toggleLike(el) {
     el.classList.toggle('fa-regular');
     el.classList.toggle('fa-solid');
@@ -37,45 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   });
-=======
-function toggleLike(el) {
-  el.classList.toggle('fa-regular');
-  el.classList.toggle('fa-solid');
-  el.classList.toggle('liked');
-}
 
-
-// navbar--section__menu displays 
-
-document.addEventListener("DOMContentLoaded", function () {
-  const popup = document.getElementById("popup");
-  const openBtn = document.getElementById("menu_id");
-  const closeBtn = document.getElementById("close");
-
-  if (popup && openBtn && closeBtn) {
-    openBtn.addEventListener("click", () => {
-      popup.classList.remove("hidden");
-      popup.classList.add("show");
-    });
-
-    closeBtn.addEventListener("click", () => {
-      popup.classList.add("hidden");
-      popup.classList.remove("show");
-    });
-  }
-});
-
-//discount--section__close 
-document.addEventListener("DOMContentLoaded", function () {
-  const banner = document.getElementById("discount--section");
-  const closeBtn = document.getElementById("discount--section__close");
-
-  if (banner && closeBtn) {
-    closeBtn.addEventListener("click", function () {
-      banner.style.display = "none"; // or use .classList.add("hidden") if using a hidden class
-    });
-  }
-});
 
 // Search functionality
 
@@ -97,4 +58,3 @@ search.addEventListener("keyup",function(){
     }
     }
 })
->>>>>>> 53acc67 (Updated search functionality)
